6a479450d34cbf41b5184c3e472269f588fb007b
Chirag Malhotra <cmalhotradelhi@gmail.com>
2024-04-07 18:40:41 -0400

    Kanbas done till signup

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ import cors from 'cors';
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import { mongoose } from 'mongoose';
+import UserRoutes from './Kanbas/Users/routes.js';
+
+mongoose.connect('mongodb://127.0.0.1:27017/kanbas');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 Lab5(app);
@@ -13,4 +18,5 @@ Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
